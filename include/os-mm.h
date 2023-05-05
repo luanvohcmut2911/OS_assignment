7c9ed14f93f4aa0f3cce9d6cf55c632e797cc33b
Nguyen Kieu Bao Khanh <khanh.nguyen1701@hcmut.edu.vn>
2023-05-05 13:43:06 +0700

    update info from pdf

--- include/os-mm.h
+++ include/os-mm.h
@@ -9,15 +9,16 @@ typedef char BYTE;
 typedef uint32_t addr_t;
 //typedef unsigned int uint32_t;
 
-struct pgn_t{
+struct pgn_t{     //
    int pgn;
    struct pgn_t *pg_next; 
 };
 
 /*
  *  Memory region struct
+ MEMORY_MANAGEMENT ---> VIRTUAL_MEM_AREA + LIST_OF_REGIONS + LIST_OF_FREE_PAGES
  */
-struct vm_rg_struct {
+struct vm_rg_struct {   //virtual memory region
    unsigned long rg_start;
    unsigned long rg_end;
 
@@ -26,35 +27,45 @@ struct vm_rg_struct {
 
 /*
  *  Memory area struct
+ virtual memory space is organized as a memory mapping for each process PCB. 
+ From the process point of view: 
+ * the virtual address includes multiple vm areas (contiguously)
+ * vm areas consist of many virtual memory region
  */
 struct vm_area_struct {
    unsigned long vm_id;
-   unsigned long vm_start;
+   unsigned long vm_start; //Memory Area: Each memory area ranges continuously in [vm start,vm end]
    unsigned long vm_end;
 
-   unsigned long sbrk;
+   unsigned long sbrk;  //The actual usable area
 /*
  * Derived field
  * unsigned long vm_limit = vm_end - vm_start
  */
    struct mm_struct *vm_mm;
-   struct vm_rg_struct *vm_freerg_list;
-   struct vm_area_struct *vm_next;
+   struct vm_rg_struct *vm_freerg_list;   //virtual FREE memory region (free slots tracking)
+   struct vm_area_struct *vm_next;     //Pointer to the next vm_area, for future set tracking
 };
 
 /* 
  * Memory management struct
  */
 struct mm_struct {
-   uint32_t *pgd;
+   uint32_t *pgd; //Page table directory, contains all page table entries
+   //* Each entry is a map between page number and the frame number in the paging mem management system.
 
-   struct vm_area_struct *mmap;
 
-   /* Currently we support a fixed number of symbol */
-   struct vm_rg_struct symrgtbl[PAGING_MAX_SYMTBL_SZ];
+   struct vm_area_struct *mmap;  
+
+   /* Currently we support a fixed number of symbol 
+   !We temporarily imagine these regions as a set of limit number of region. We manage them by using an array of symrgtbl[PAGING MAX SYMTBL SZ].
+   */
+   struct vm_rg_struct symrgtbl[PAGING_MAX_SYMTBL_SZ];   
+   //?PAGING_MAX_SYMTBL_SZ: Paging Max Symbol Table Size (i'm so tired of this type of abbreviation)
+   //?symrgtbl = symbol region table
 
    /* list of free page */
-   struct pgn_t *fifo_pgn;
+   struct pgn_t *fifo_pgn; //![JUST LEFT IT THERE] mainly used to keep track of a specific user operation (caller, fifo page...) [jor refs]
 };
 
 /*
